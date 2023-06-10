15bff9c6105eb0fb810664b5859fa54121ddd9eb
AbduraimovErlan <96403251+AbduraimovErlan@users.noreply.github.com>
2023-06-10 09:40:09 -0400

    improve

--- movies/serializers.py
+++ movies/serializers.py
@@ -1,4 +1,5 @@
 from rest_framework import serializers
+from rest_framework.exceptions import ValidationError
 
 from movies.models import Movie, Director, Genre, Review
 
@@ -38,3 +39,34 @@ class MovieRetrieveSerializer(serializers.ModelSerializer):
     class Meta:
         model = Movie
         fields = '__all__'
+
+""" 4 step validate of data """
+class MovieValidateSerializer(serializers.Serializer):
+    director_id = serializers.IntegerField(required=False)
+    title = serializers.CharField(min_length=1, max_length=100)
+    description = serializers.CharField()
+    rate = serializers.FloatField(min_value=1)
+    genres = serializers.ListField(child=serializers.IntegerField(min_value=1))
+
+    def validate_genres(self, genres):
+        genres_db = Genre.objects.filter(id__in=genres)
+        if len(genres_db) != len(genres):
+            raise ValidationError('Genre not found')
+        return genres
+
+
+
+    def validate_director_id(self, director_id):
+        try:
+            Director.objects.get(id=director_id)
+        except Director.DoesNotExist:
+            raise ValidationError(f'Director with if {director_id} not found')
+        return director_id
+
+
+    # def validate(self, attrs):
+    #     try:
+    #         Director.objects.get(id=attrs['director_id'])
+    #     except Director.DoesNotExist:
+    #         raise ValidationError(f'Director with if ({attrs["director_id"]}) not found')
+    #     return attrs
--- movies/views.py
+++ movies/views.py
@@ -3,7 +3,7 @@ from rest_framework import status
 from rest_framework.response import Response
 
 from movies.models import Movie
-from movies.serializers import MovieSerializer, MovieRetrieveSerializer
+from movies.serializers import MovieSerializer, MovieRetrieveSerializer, MovieValidateSerializer
 
 
 @api_view(['GET'])
@@ -22,8 +22,12 @@ def movie_list_create_api_view(request):
         return Response(data=data, status=status.HTTP_200_OK)  # return data
 
     if request.method == 'POST':
-        data = request.data
-
+        """1 step: validation of data """
+        serializer = MovieValidateSerializer(data=request.data)
+        if not serializer.is_valid():
+            return Response(status=status.HTTP_406_NOT_ACCEPTABLE, data=serializer.errors)
+        """ 2 step: create object(operation)"""
+        data = serializer.validated_data
         movie = Movie.objects.create(
             director_id=data.get('director_id'),
             title=data.get('title'),
@@ -31,7 +35,7 @@ def movie_list_create_api_view(request):
             rate=data.get('rate'),
         )
         movie.genres.set(data.get('genres'))
-
+        """3 step: return response """
         return Response(data=MovieSerializer(movie, many=False).data, status=status.HTTP_201_CREATED)
 
 @api_view(['GET', 'PUT'])
@@ -45,6 +49,8 @@ def movie_retrieve_api_view(request, **kwargs):
         return Response(data=data, status=status.HTTP_200_OK)
 
     if request.method == 'PUT':
+        serializer = MovieValidateSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
         data = request.data
 
         movie.director_id = data.get('director_id')
