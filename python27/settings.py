b2f929d1dcc5e38948fee32d1f554a367f53594f
AbduraimovErlan <96403251+AbduraimovErlan@users.noreply.github.com>
2024-01-06 16:14:33 -0500

    improve to deploy

--- python27/settings.py
+++ python27/settings.py
@@ -90,8 +90,13 @@ WSGI_APPLICATION = 'python27.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'python27_db',
+        'USER': 'python27_user',
+        'PASSWORD': '123',
+        'HOST': 'localhost',
+        'PORT': '5432',
+
     }
 }
 
